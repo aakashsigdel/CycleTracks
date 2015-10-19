a9659ce521e7b2a498ec1a871325835013e9a735
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-10-19 18:53:04 +0545

    added elevation data and coloring of polyline when clicked

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
+// Overpass query to fetch cycle tracks
 var QUERY_URL = 'http://overpass-api.de/api/interpreter?data=' + 
-			'(way(%minLat%,%minLng%,%maxLat%,%maxLng%)' + 
-				';);(._;>;);out body;'
+			'(way["highway"="path"]["bicycle"!~"no"](%minLat%,%minLng%,%maxLat%,%maxLng%);' +
+			'way["highway"="track"]["bicycle"!~"no"](%minLat%,%minLng%,%maxLat%,%maxLng%););' +
+			'(._;>;);out body;'
 
-var layer;
+
+// Cycle layers
+var layers;
+// Previously clicked cycle track on Map
+var prevLayer = null;
+// Currently clicked layer
+var currLayer = null;
+// lat lang array of currently selected polyline(Cycle track)
+var currLatLngArr = [];
+
 $(function() {
+	var map = initializeTracks();
+	setUpEventHandellers(map);
+});
+
+// initailze the map with tracks
+function initializeTracks() {
 	var map = L.map('map').setView([47.507817, -121.739877], 14);
+
+	L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, ' + 
+				'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+	return map;
+}
+
+// setup all event handellers like zoom panned loaded
+function setUpEventHandellers(map) {
 	var replacement = {
 		"%minLat%": map.getBounds()._southWest.lat,
 		"%minLng%":  map.getBounds()._southWest.lng,
@@ -12,51 +40,94 @@ $(function() {
 		"%maxLng%": map.getBounds()._northEast.lng
 	};
 
-	L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
+	var query = getQuery(QUERY_URL, replacement);
 
-	initializeTracks(map, replacement);
+	// Temporary Click handller to check clear leayrs
 	$('.home').click(function() {
-		layer.clearLayers();
+		layers.clearLayers();
 	});
 
+	// Load inital data on the map and sepup clicks on the  tracks
+	$.get(query, function(data) {
+		layers =  new L.OSM.DataLayer(data).addTo(map);
+		layers.on('click', function(layer) {
+			currLayer = layer;
+			
+			// lat lng array of polyline points
+			currLatLngArr = L.polyline(currLayer.layer._latlngs).getLatLngs();
+
+			map.fitBounds(layer.layer.getBounds());
+			if(prevLayer === null)
+				layer.layer.setStyle({ color: 'red' });
+			else {
+				prevLayer.layer.setStyle({ color: 'blue' });
+				layer.layer.setStyle({ color: 'red' });
+			}
+			prevLayer = layer;
+			getElevationAndDistance(currLatLngArr[currLatLngArr.length - 1], currLatLngArr[0]);
+		});
+	});
+
+	// When map is zoomed greater than 16 level
 	map.on('zoomend', function() {
 		if(map.getZoom() > 16) {
-			var query = getQuery(replacement);
-			console.log(query);
+			if(currLayer !== null)
+				currLayer.layer.setStyle({ color: 'red' });
+			
 			$.get(query, function(data) {
-				console.log('hello');
-				layer.clearLayers();
-				layer =  new L.OSM.DataLayer(data);
-				layer.bindPopup('hello').addTo(map);
-				layer.on('click', function(temp) {
-					map.fitBounds(temp.layer.getBounds());
-					temp.layer.setStyle({ color: 'red' });
+				layers =  new L.OSM.DataLayer(data);
+				layers.on('click', function(layer) {
+					currLayer = layer;
+					
+					// lat lng array of polyline points
+					currLatLngArr = L.polyline(currLayer.layer._latlngs).getLatLngs();
+
+					map.fitBounds(layer.layer.getBounds());
+					layer.layer.setStyle({ color: 'red' });
+					if(prevLayer === null)
+						layer.layer.setStyle({ color: 'red' });
+					else {
+						prevLayer.layer.setStyle({ color: 'blue' });
+						layer.layer.setStyle({ color: 'red' });
+					}
+					prevLayer = layer;
 				});
 			});
 		}
 	});
-});
 
-function initializeTracks(map, replacement) {
-	var query = getQuery(replacement);
-	console.log(query);
-	//if(map.getZoom() > 15) {
-		$.get(query, function(data) {
-			layer =  new L.OSM.DataLayer(data).addTo(map);
-			layer.on('click', function(temp) {
-				map.fitBounds(temp.layer.getBounds());
-				temp.layer.setStyle({ color: 'red' });
-			});
-			console.log(layer);
-		});
-	//}
 }
 
-function getQuery(replacement) {
-	query = QUERY_URL.replace(/%\w+%/g, function(all) {
+// make query with bounding box
+function getQuery(query, replacement) {
+	query = query.replace(/%\w+%/g, function(all) {
 		return replacement[all] || all;
 	});
 	return query;
+}
+
+function getElevationAndDistance(start, end) {
+	var ELEVATION_URL = 'http://open.mapquestapi.com/elevation/v1/profile?key=%api_key%' + 
+			'&shapeFormat=raw&latLngCollection=%startLat%,%startLng%,%endLat%,%endLng%';
+
+	var replacement = {
+		'%api_key%': '9JZljemB2v3GJTizHcjbDz3eB32omiwv',
+		"%startLat%": start.lat,
+		"%startLng%":  start.lng,
+		"%endLat%": end.lat,
+		"%endLng%": end.lng
+	};
+	var query = getQuery(ELEVATION_URL, replacement);
+	console.log(query);
+	$.get(query, function(data) {
+		$('.details>.elevation>.from>.value').html(data.elevationProfile[0].height + 
+																							 ' meters above sea level');
+		$('.details>.elevation>.to>.value').html(data.elevationProfile[1].height + 
+																						 ' meters above sea level');
+		$('.details>.distance>.value')
+			.html(Math.round(Number(data.elevationProfile[1].distance) * 100000) / 100  + 'meters');
+		console.log('hello');
+		console.log(data.elevationProfile);
+
+	});
 }
